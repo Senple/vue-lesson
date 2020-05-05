423fdb391999db6979069d4065015597a3d12a3e
senple <semple.python@gmail.com>
2020-05-05 14:27:02 +0900

    P.65 イベントハンドラーに任意の引数を渡す。

--- js/hello.js
+++ js/hello.js
@@ -18,6 +18,7 @@ let app = new Vue({
         onclick: function () {
             this.message = new Date().toLocaleString();
         },
+        // p.58 mouse in/outの使い方
         // mouseenter/mouseleaveイベントの情報をresultに反映
         onmousein: function (e) {
             this.result += 'Enter:' + e.target.id + '<br />';
@@ -25,6 +26,7 @@ let app = new Vue({
         onmouseout: function (e) {
             this.result += 'Leave:' + e.target.id + '<br />';
         },
+        // P.64 mouse位置を取得して、マウス位置を可視化する。
         onmousemove: function (e) {
             this.screenX = e.screenX;
             this.screenY = e.screenY;
@@ -34,6 +36,11 @@ let app = new Vue({
             this.clientY = e.clientY;
             this.offsetX = e.offsetX;
             this.offsetY = e.offsetY;
+        },
+        // P.65 イベントハンドラーに任意の引数を渡す。 
+        onclick65: function (message, e) {
+            console.log(message);
+            console.log(e);
         }
     }
 })
