7504a4fd23d1c0226ac98b708af0e889ecf420f1
senple <semple.python@gmail.com>
2020-05-05 13:42:39 +0900

    p.58 mouse in/outの使い方

--- js/hello.js
+++ js/hello.js
@@ -4,11 +4,19 @@ let app = new Vue({
         message: '皆さん、こんにちは！',
         url: 'https://wings.msn.to/',
         flag: false,
+        result: '',
 
     },
     methods: {
         onclick: function () {
             this.message = new Date().toLocaleString();
-        }
+        },
+        // mouseenter/mouseleaveイベントの情報をresultに反映
+        onmousein: function (e) {
+            this.result += 'Enter:' + e.target.id + '<br />';
+        },
+        onmouseout: function (e) {
+            this.result += 'Leave:' + e.target.id + '<br />';
+        },
     }
 })
