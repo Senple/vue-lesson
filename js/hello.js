837e0635d0811e29036931f0847cdfd7d89c4e62
senple <semple.python@gmail.com>
2020-05-05 13:56:46 +0900

    P.64 mouse位置を取得して、マウス位置を可視化する。

--- js/hello.js
+++ js/hello.js
@@ -5,7 +5,14 @@ let app = new Vue({
         url: 'https://wings.msn.to/',
         flag: false,
         result: '',
-
+        screenX: 0,
+        screenY: 0,
+        pageX: 0,
+        pageY: 0,
+        clientX: 0,
+        clientY: 0,
+        offsetX: 0,
+        offsetY: 0,
     },
     methods: {
         onclick: function () {
@@ -18,5 +25,15 @@ let app = new Vue({
         onmouseout: function (e) {
             this.result += 'Leave:' + e.target.id + '<br />';
         },
+        onmousemove: function (e) {
+            this.screenX = e.screenX;
+            this.screenY = e.screenY;
+            this.pageX = e.pageX;
+            this.pageY = e.pageY;
+            this.clientX = e.clientX;
+            this.clientY = e.clientY;
+            this.offsetX = e.offsetX;
+            this.offsetY = e.offsetY;
+        }
     }
 })
