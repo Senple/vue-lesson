1f82f31250106f69027d94c19272c10fe9e7942f
senple <semple.python@gmail.com>
2020-05-09 16:50:07 +0900

    P.151 コンポーネント間の値の受け渡し

--- js/lesson4.js
+++ js/lesson4.js
@@ -1,23 +1,10 @@
-// P.143 コンポーネントの基本
-Vue.component('my-Hello', {
-    template: '<div>こんにちは、{{ name }} !</div>',
-    data: function () {
-        return {
-            name: 'Vue.js'
-        };
-    }
+// P.151 コンポーネント間の値の受け渡し
+Vue.component('my-hello', {
+    // プロパティを定義
+    props: ['yourName'],
+    template: '<div>こんにちは、{{ yourName }} さん !</div>',
 });
-
-// P.147グローバル登録とローカル登録  
-let MyHello = {
-    template: '<div>こんにちは、Vue.js !</div>',
-};
 
 new Vue({
     el: "#app",
-    // P.147グローバル登録とローカル登録 
-    // コンポーネントをローカル登録
-    components: {
-        'my-hello': MyHello
-    }
 });
