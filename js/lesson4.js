c7b5af7fa54e3287d23c3eab4175c6694298fa2f
senple <semple.python@gmail.com>
2020-05-09 22:07:04 +0900

    P.152 プロパティで受け取った値を更新する

--- js/lesson4.js
+++ js/lesson4.js
@@ -3,6 +3,27 @@ Vue.component('my-hello', {
     // プロパティを定義
     props: ['yourName'],
     template: '<div>こんにちは、{{ yourName }} さん !</div>',
+
+});
+
+// P.152 プロパティで受け取った値を更新する
+// Vue componentは一つのcomponentごとに、vue.componentと書く必要がある。
+Vue.component('my-counter', {
+    props: ['init'],
+    template: '<div>現在地は {{ current }} です！ \
+    <input type="button" v-on:click="onclick" value="増やす" /></div>',
+    // データオブジェクトを定義
+    data: function () {
+        return {
+            current: this.init
+        };
+    },
+    methods: {
+        // クリック時にcurrentプロパティをインクリメント
+        onclick: function () {
+            this.current++;
+        }
+    }
 });
 
 new Vue({
