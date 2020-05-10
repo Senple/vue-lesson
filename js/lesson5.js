89348a4a0f37d9efa968e0d7a11b70dfaacaaaf1
senple <semple.python@gmail.com>
2020-05-10 19:55:11 +0900

    P.210 リストトランジション

--- js/lesson5.js
+++ js/lesson5.js
@@ -1,12 +1,28 @@
-// P.205 複数のアニメーションを同居させる 
+// P.210 リストトランジション
 new Vue({
-    el: '#app',
+    el: "#app",
     data: {
-        flag: true
+        todo: '',
+        // Todo list
+        items: [
+            'A書籍の構成案作成',
+            'X記事の著者校正',
+            '今月末の請求書作成',
+            'WINGSメンバーの面接'
+        ]
     },
     methods: {
-        onclick: function () {
-            this.flag = !this.flag;
-        }
+        // 新たに入力された項目は配列の先頭に追加
+        onadd: function () {
+            this.items.unshift(this.todo);
+            this.todo = '';
+        },
+        onremove: function () {
+            let that = this;
+            this.items = this.items.filter(function (value) {
+                return value !== that.todo;
+            });
+            this.todo = '';
+        },
     }
 });
