9b34caa338e7b81b640266886e1f42451e21b497
senple <semple.python@gmail.com>
2020-05-10 19:11:05 +0900

    .201 key属性をトリガーとしたアニメーション

--- js/lesson5.js
+++ js/lesson5.js
@@ -1,13 +1,20 @@
-// P.190 アニメーションの基本 
+// P.201 key属性をトリガーとしたアニメーション
 new Vue({
     el: "#app",
     data: {
-        flag: true
+        // 表示するパネル(インデックス番号)
+        id: 0,
+        // 表示パネルを配列として
+        panels: [
+            "WINGSプロジェクトはライター...",
+            "山田氏の書籍に関するFAQのための",
+            "環境設定に関しては、ページ上部に・・・"
+        ]
     },
     methods: {
         // ボタンクリック時に表示状態をオンオフ
         onclick: function () {
-            this.flag = !this.flag;
+            this.id = (this.id + 1) % this.panels.length;
         }
     },
 });
