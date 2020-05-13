59adb5b570a940a6c93f8bbe5d0a22c69366a24e
senple <semple.python@gmail.com>
2020-05-14 03:07:58 +0900

    P.228 属性値の変化を検出する。

--- js/lesson6.js
+++ js/lesson6.js
@@ -1,8 +1,14 @@
-// P.225 ディレクティブの基本
+// P.228 属性値の変化を検出する。
 Vue.directive('highlight', {
+    // 紐づいた時の処理（初回だけ)
     bind: function (el, binding, vnode, oldVnode) {
         el.style.backgroundColor = binding.value;
+    },
+    // 上位コンポーネントが変化した時
+    update: function (el, binding, vnode, oldVnode) {
+        el.style.backgroundColor = binding.value;
     }
+
 })
 
 new Vue({
