0a2a51e942b27ae5419c4aad177d883fa354ad69
senple <semple.python@gmail.com>
2020-05-14 23:26:37 +0900

    P.234 引数付きのディレクティブを定義する。

--- js/lesson6.js
+++ js/lesson6.js
@@ -1,29 +1,28 @@
-// P.233 修飾子付きのディレクティブを定義する。
-// bind/updateに適用するフック関数
-let hook = function (el, binding) {
-    if (binding.value === binding.oldValue) { return; }
-    // border修飾子で背景ハイライトが枠線ハイライトかを選択
-    if (binding.modifiers.border) {
-        el.style.borderColor = binding.value;
-        el.style.borderStyle = "solid";
-    } else {
-        el.style.backgroundColor = binding.value;
-    }
-};
-Vue.directive('highlight', {
-    bind: hook,
-    update: function (el, binding, vnode, oldVnode) {
-        // once修飾子でupdate更新はスキップ
-        if (binding.modifiers.once) {
-            hook(el, binding)
-            // elは、<span border-color:"red"; border-style:"solid";>メンバー募集中！</span>
-        }
+// P.234 引数付きのディレクティブを定義する。
+Vue.directive('highlight', function (el, binding, vnode, oldVnode) {
+    // 引数の値に応じてハイライト処理を分岐
+    // v-highlightのbindの引数を変更すると、highlight方法が変化する。
+    switch (binding.arg) {
+        case 'bg':
+            el.style.backgroundColor = binding.value;
+            break;
+        case 'border':
+            el.style.borderStyle = 'solid';
+            el.style.borderWidth = '1px';
+            el.style.borderColor = binding.value;
+            break;
+        case 'text':
+            el.style.fontweight = 'bold';
+            el.style.color = binding.value;
+            break;
+        default:
+            throw new Error('指定のハイライトを使えません。');
     }
 });
 
 new Vue({
     el: '#app',
     data: {
-        color: 'yellow'
+        color: 'red'
     }
 })
