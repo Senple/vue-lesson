1f2fc3b6345dfe1269ec5fae108999ff932c66ef
senple <semple.python@gmail.com>
2020-05-15 00:23:55 +0900

    P.238 markedライブラリをラップする。 このコードはうまく機能しなかった。 vscodeの自動開業などの問題かな？

--- js/lesson6.js
+++ js/lesson6.js
@@ -1,28 +1,31 @@
-// P.234 引数付きのディレクティブを定義する。
-Vue.directive('highlight', function (el, binding, vnode, oldVnode) {
-    // 引数の値に応じてハイライト処理を分岐
-    // v-highlightのbindの引数を変更すると、highlight方法が変化する。
-    switch (binding.arg) {
-        case 'bg':
-            el.style.backgroundColor = binding.value;
-            break;
-        case 'border':
-            el.style.borderStyle = 'solid';
-            el.style.borderWidth = '1px';
-            el.style.borderColor = binding.value;
-            break;
-        case 'text':
-            el.style.fontweight = 'bold';
-            el.style.color = binding.value;
-            break;
-        default:
-            throw new Error('指定のハイライトを使えません。');
-    }
+// // P.238 markedライブラリをラップする。
+// Vue.directive('markdown', function (el, binding, vnode, oldVnode) {
+//     // markdown文字列をHTML文字列に変換する。
+//     el.innerHTML = marked(el.textContent, binding.value);
+// });
+
+// new Vue({
+//     el: '#app',
+//     data: {
+//         options: {
+//             gfm: true,
+//             breaks: true,
+//             xhtml: true,
+//         }
+//     },
+// });
+
+Vue.directive('markdown', function (el, binding, vnode, oldVnode) {
+    el.innerHTML = marked(el.textContent, binding.value);
 });
 
 new Vue({
     el: '#app',
     data: {
-        color: 'red'
-    }
-})
+        options: {
+            gfm: true,
+            breaks: true,
+            xhtml: true,
+        }
+    },
+});
