16e0470d95b9f0ff71f9b5a1141239844b53415a
senple <semple.python@gmail.com>
2020-05-14 22:45:33 +0900

    P.233 修飾子付きのディレクティブを定義する。

--- js/lesson6.js
+++ js/lesson6.js
@@ -1,16 +1,28 @@
-// P.230 値の変化をより厳密に検知する。
-Vue.directive('highlight', function (el, binding, vnode, oldVnode) {
-    // 属性値に変化がなければ終了
+// P.233 修飾子付きのディレクティブを定義する。
+// bind/updateに適用するフック関数
+let hook = function (el, binding) {
     if (binding.value === binding.oldValue) { return; }
-    console.log(binding.value);
-    el.style.backgroundColor = binding.value;
-
+    // border修飾子で背景ハイライトが枠線ハイライトかを選択
+    if (binding.modifiers.border) {
+        el.style.borderColor = binding.value;
+        el.style.borderStyle = "solid";
+    } else {
+        el.style.backgroundColor = binding.value;
+    }
+};
+Vue.directive('highlight', {
+    bind: hook,
+    update: function (el, binding, vnode, oldVnode) {
+        // once修飾子でupdate更新はスキップ
+        if (binding.modifiers.once) {
+            hook(el, binding)
+        }
+    }
 });
 
 new Vue({
     el: '#app',
     data: {
-        name: "アルバス・ダンブルドア",
         color: 'yellow'
     }
 })
