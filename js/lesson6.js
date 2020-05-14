857ece35e243121e927088503a969b0fed92ff5f
senple <semple.python@gmail.com>
2020-05-14 22:21:56 +0900

    属性値に変化がなければ終了

--- js/lesson6.js
+++ js/lesson6.js
@@ -1,6 +1,7 @@
 // P.230 値の変化をより厳密に検知する。
 Vue.directive('highlight', function (el, binding, vnode, oldVnode) {
-    // 現在の属性値をログ出力
+    // 属性値に変化がなければ終了
+    if (binding.value === binding.oldValue) { return; }
     console.log(binding.value);
     el.style.backgroundColor = binding.value;
 
