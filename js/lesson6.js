a349e465663325011688d2d1e5f82e3b3c65be28
senple <semple.python@gmail.com>
2020-05-18 01:27:43 +0900

    P.262 ミックスインの基本

--- js/lesson6.js
+++ js/lesson6.js
@@ -1,4 +1,21 @@
-// P.258 カルーセルの実装。
+// P.262 ミックスインの基本。
+let dataLoggle = {
+    mounted: function () {
+        console.log(this.$data);
+    }
+};
+
+Vue.component('my-comp', {
+    data: function () {
+        return {
+            current: new Date()
+        }
+    },
+    template: `<div> 現在時刻：{{ current }} </div>`,
+    // ミックスインを組み込み
+    mixins: [dataLoggle]
+});
+
 new Vue({
     el: '#app',
 });
